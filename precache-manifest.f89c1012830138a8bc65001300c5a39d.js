e644639b59f83f0b209c41f151bfdf11efb93249
Ashmeet Sehgal <asehgal@tekion.com>
2019-05-25 15:03:31 +0530

    test with cra

---
+++ precache-manifest.f89c1012830138a8bc65001300c5a39d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a5ab56f107a6d1e9d83dfb662630a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e89821818c6bffa036a8",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "e89821818c6bffa036a8",
+    "url": "/static/js/main.bc765d81.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
